635f428f049f104ffdd6c3e92fb76c217e47c309
小冷睡着了. <2455880592@qq.com>
2024-07-31 18:32:13 +0800

    :bug: fix：ts类型语法错误

--- packages/web/src/store/hooks/useTheme/useGetters.ts
+++ packages/web/src/store/hooks/useTheme/useGetters.ts
@@ -7,7 +7,7 @@ export const useGetters = (state: ThemeState) => {
   // 获取菜单列表，用于渲染左侧菜单栏，以及 tags，header，侧边栏抽屉，面包屑，路由，权限，动态路由，动态权限，动态菜单
   const getMenus = computed(() => {
     const menus = $local.get('BREADCRUMBKEYFORPM')
-    return state.menus || menus || [];
+    return state.cacheMenus || menus || [];
   });
   // 获取终端类型 mobile pc
   const terminalType = computed(() => getTerminal());
