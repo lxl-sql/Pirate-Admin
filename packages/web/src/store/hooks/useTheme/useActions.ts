236371db1b3b8da79e350f903f34e5fb9c67bc27
小冷睡着了. <2455880592@qq.com>
2024-08-01 13:01:51 +0800

    :bug: fix：ts类型语法错误

--- packages/web/src/store/hooks/useTheme/useActions.ts
+++ packages/web/src/store/hooks/useTheme/useActions.ts
@@ -208,7 +208,7 @@ export const useActions = (state: ThemeState) => {
         siderMenus.value = []
       } else {
         const key = _route.name
-        let newSiderMenus: object | null = {}
+        let newSiderMenus: any | null = {}
         treeForEach(_cacheMenus, (menu, _index, _arr, parent) => {
           if (menu.name === key) {
             newSiderMenus = parent ? parent : menu
