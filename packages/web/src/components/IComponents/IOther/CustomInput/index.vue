78cd43598fce3f49e7ccd05c5a4c3060806bc1fe
小冷睡着了. <2455880592@qq.com>
2024-08-28 18:09:40 +0800

    :recycle: reflector: CustomInput - 重构: 优化input的加载方式

--- packages/web/src/components/IComponents/IOther/CustomInput/index.vue
+++ packages/web/src/components/IComponents/IOther/CustomInput/index.vue
@@ -1,10 +1,29 @@
 <script setup lang="ts">
-import {withDefaults} from 'vue'
-import {IColumns} from "@/types";
-import {DatePickerProps, TimePicker as ATimePicker} from 'ant-design-vue';
+import {computed, useAttrs, withDefaults} from 'vue'
+import {FormType} from "@/types";
+import {
+  Cascader,
+  CheckboxGroup,
+  DatePicker,
+  DatePickerProps,
+  Input,
+  InputNumber,
+  InputPassword,
+  RadioGroup,
+  RangePicker,
+  Select,
+  Switch,
+  Textarea,
+  TimePicker,
+  Tree
+} from 'ant-design-vue';
+import ITreeSelect from "@/components/IComponents/ITreeSelect/index.vue";
+import KeyValue from "@/components/IComponents/IInput/components/KeyValue/index.vue";
+import IIcon from "@/components/IComponents/IIcon/index.vue";
+import IUpload from '@/components/IComponents/IUpload/index.vue'
 
 interface CustomInputProps {
-  type?: IColumns['type'],
+  type?: FormType,
   options?: any[],
   picker?: DatePickerProps['picker']
   checkedKeys?: any
@@ -12,123 +31,95 @@ interface CustomInputProps {
   value?: any
 }
 
-withDefaults(defineProps<CustomInputProps>(), {});
+const props = withDefaults(defineProps<CustomInputProps>(), {});
+const $attrs = useAttrs();
+
+const componentType = computed(() => {
+  const componentMap: Record<FormType, any> = {
+    input: Input,
+    'input-password': InputPassword,
+    'input-number': InputNumber,
+    textarea: Textarea,
+    select: Select,
+    tree: Tree,
+    'tree-select': ITreeSelect,
+    cascader: Cascader,
+    checkbox: CheckboxGroup,
+    radio: RadioGroup,
+    'radio-button': RadioGroup,
+    'date-picker': DatePicker,
+    'range-picker': RangePicker,
+    'time-picker': TimePicker,
+    upload: IUpload,
+    'key-value': KeyValue,
+    switch: Switch,
+    editor: Input,
+    city: Input,
+    color: Input,
+    icon: IIcon,
+  };
+  return componentMap[props.type || 'input'];
+});
+
+const dynamicProps = computed(() => {
+  let baseProps: object | undefined;
+
+  switch (props.type) {
+    case 'input':
+    case 'input-password':
+    case 'input-number':
+    case 'textarea':
+    case 'color':
+      baseProps = {value: props.value, allowClear: true};
+      break;
+
+    case 'select':
+    case 'tree-select':
+    case 'cascader':
+    case 'radio':
+    case 'radio-button':
+      baseProps = {value: props.value, options: props.options, allowClear: true};
+      break;
+
+    case 'tree':
+      baseProps = {treeData: props.options, checkedKeys: props.checkedKeys};
+      break;
+
+    case 'upload':
+      baseProps = {fileList: props.fileList};
+      break;
+
+    case 'checkbox':
+      baseProps = {value: props.value};
+      break;
+
+    case 'date-picker':
+    case 'range-picker':
+    case 'time-picker':
+      baseProps = {value: props.value, picker: props.picker};
+      break;
+
+    case 'switch':
+    case 'icon':
+    case 'key-value':
+      baseProps = {value: props.value};
+      break;
+
+    default:
+      baseProps = {}; // 确保至少返回一个空对象
+      break;
+  }
+
+  return {...baseProps, ...$attrs};
+});
+
 </script>
 
 <template>
-  <a-input
-    v-if="!type || type === 'input'"
-    allow-clear
-    :value="value"
-    v-bind="$attrs"
-  />
-  <a-input-number
-    v-else-if="type === 'input-number'"
-    allow-clear
-    :value="value"
-    v-bind="$attrs"
-  />
-  <a-input-password
-    v-else-if="type === 'input-password'"
-    allow-clear
-    :value="value"
-    v-bind="$attrs"
-  />
-  <a-textarea
-    v-else-if="type === 'textarea'"
-    allow-clear
-    :rows="3"
-    :value="value"
-    v-bind="$attrs"
-  />
-  <i-tree-select
-    v-else-if="type === 'tree-select'"
-    allow-clear
-    :tree-data="options"
-    :value="value"
-    v-bind="$attrs"
-  />
-  <a-tree
-    v-else-if="type === 'tree'"
-    :tree-data="options"
-    :checked-keys="checkedKeys"
-    v-bind="$attrs"
-  />
-  <a-select
-    v-else-if="type === 'select'"
-    allow-clear
-    :options="options"
-    :value="value"
-    v-bind="$attrs"
-  />
-  <a-cascader
-    v-else-if="type === 'cascader'"
-    allow-clear
-    :options="options"
-    :value="value"
-    v-bind="$attrs"
-  />
-  <i-upload
-    v-else-if="type === 'upload'"
-    :file-list="fileList"
-    v-bind="$attrs"
-  />
-  <a-checkbox-group
-    v-else-if="type === 'checkbox'"
-    :value="value"
-    v-bind="$attrs"
-  />
-  <a-radio-group
-    v-else-if="type === 'radio'"
-    :options="options"
-    :value="value"
-    v-bind="$attrs"
-  />
-  <a-radio-group
-    v-else-if="type === 'radio-button'"
-    option-type="button"
-    :options="options"
-    :value="value"
-    v-bind="$attrs"
-  />
-  <a-date-picker
-    v-else-if="type === 'date-picker'"
-    :picker="picker"
-    :value="value"
-    v-bind="$attrs"
-  />
-  <a-range-picker
-    v-else-if="type === 'range-picker'"
-    :picker="picker"
-    :value="value"
-    v-bind="$attrs"
-  />
-  <a-time-picker
-    v-else-if="type === 'time-picker'"
-    :picker="picker"
-    :value="value"
-    value-format="HH:mm:ss"
-  />
-  <a-switch
-    v-else-if="type === 'switch'"
-    :value="value"
-    v-bind="$attrs"
-  />
-  <i-icon
-    v-else-if="type === 'icon'"
-    :value="value"
-    v-bind="$attrs"
-  />
-  <a-input
-    v-if="type === 'color'"
-    type="color"
-    allow-clear
-    :value="value"
-    v-bind="$attrs"
-  />
-  <key-value
-    v-if="type === 'key-value'"
-    :value="value"
-    v-bind="$attrs"
-  />
+  <component
+    :is="componentType"
+    v-bind="dynamicProps"
+  >
+    <slot></slot>
+  </component>
 </template>
