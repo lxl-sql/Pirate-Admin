0efc524fa8b155b3066e8b291d59b43799831214
小冷睡着了. <2455880592@qq.com>
2024-09-08 10:21:46 +0800

    :rocket: feat: `server` - 新增 `winston` 自定义日志

--- packages/server/src/common/logger/app-logger.service.ts
+++ packages/server/src/common/logger/app-logger.service.ts
@@ -10,19 +10,19 @@ export class AppLogger implements LoggerService {
     this.logger = createLogger(options)
   }
 
-  public log(message: string, context: string) {
+  public log(message: string, context?: string) {
     const time = dayjs(Date.now()).format('YYYY-MM-DD HH:mm:ss');
 
     this.logger.log('info', message, {context, time})
   }
 
-  public error(message: string, context: string) {
+  public error(message: string, context?: string) {
     const time = dayjs(Date.now()).format('YYYY-MM-DD HH:mm:ss');
 
     this.logger.log('info', message, {context, time})
   }
 
-  public warn(message: string, context: string) {
+  public warn(message: string, context?: string) {
     const time = dayjs(Date.now()).format('YYYY-MM-DD HH:mm:ss');
 
     this.logger.log('info', message, {context, time})
--- packages/server/src/modules/cron/cron/cron.service.ts
+++ packages/server/src/modules/cron/cron/cron.service.ts
@@ -1,19 +1,52 @@
-import {Injectable} from '@nestjs/common';
+import {Inject, Injectable, OnModuleDestroy, OnModuleInit} from '@nestjs/common';
+import {InjectRepository} from "@nestjs/typeorm";
+import {Repository} from "typeorm";
+import {CronJob} from "cron";
+import {WINSTON_LOGGER_TOKEN} from "@/const/winston.const";
+import {AppLogger} from "@/common/logger/app-logger.service";
+import {Cron} from "./entities/cron.entity";
 import {CreateCronDto} from './dto/create-cron.dto';
 import {UpdateCronDto} from './dto/update-cron.dto';
-import {InjectRepository} from "@nestjs/typeorm";
-import {Cron} from "@/modules/cron/cron/entities/cron.entity";
-import {Repository} from "typeorm";
 
 @Injectable()
-export class CronService {
+export class CronService implements OnModuleInit, OnModuleDestroy {
   @InjectRepository(Cron)
   private readonly cronRepository: Repository<Cron>
+
+  @Inject(WINSTON_LOGGER_TOKEN)
+  private readonly logger: AppLogger;
+
+  private jobs: CronJob[] = []
+
+  async onModuleInit() {
+    const crons = await this.cronRepository.find()
+    crons.forEach(cron => {
+      const cornTime = cron.cron
+      const job = new CronJob(cornTime, () => {
+
+      })
+
+      job.start();
+      this.jobs.push(job); // 将任务保存到 jobs 数组中
+    })
+  }
+
+  onModuleDestroy() {
+    this.clearJobs()
+  }
+
+  private clearJobs() {
+    this.jobs.forEach((job) => job.stop());
+    this.jobs = [];
+    this.logger.log('All scheduled jobs have been stopped and cleared.');
+  }
 
   async create(createCronDto: CreateCronDto) {
     const newCron = this.cronRepository.create(createCronDto)
 
-    return await this.cronRepository.save(newCron)
+    await this.cronRepository.save(newCron)
+
+    return '定时任务创建成功'
   }
 
   findAll() {
